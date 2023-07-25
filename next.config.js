28764edd454f28e86e6b825a7995a1538ca8a74b
Seonghyun Yang <seonghyuny1023@naver.com>
2023-07-25 19:19:08 +0900

    add naver map&search api

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    NAVER_MAP_CLIENT_ID: 'oe1nc3j5z5',
+    NAVER_API_CLIENT_ID: 'FaJ_YB8ZZJCnUfK_WCDH',
+    NAVER_API_KEY_SECRET: 'mbR8UKw1UP',
+  }
 }
 
 module.exports = nextConfig
--- pages/plan.js
+++ pages/plan.js
@@ -1,27 +1,37 @@
-import React from "react"
+import React, {useState} from "react"
 import { Tabs, TabList, Tab, TabPanels, TabPanel } from "@chakra-ui/react"
 import { Container } from "@chakra-ui/react"
-import Layout from "../components/Layout"
+import Map from "../components/Map"
+import MapSearch from "../components/MapSearch"
 
 export default function Plan() {
+
+  const [selectedLocation, setSelectedLocation] = useState(null);
+
+  const handleLocationSelect = (location) => {
+    setSelectedLocation(location);
+  };
+
   return (
-    <Layout>
-      <Container centerContent paod>
-        <Tabs variant='soft-rounded' colorScheme='blue'>
-          <TabList>
-            <Tab>나의 계획</Tab>
-            <Tab>일행과 비교하기</Tab>
-          </TabList>
-          <TabPanels>
-            <TabPanel>
+    <Container centerContent>
+      <Tabs variant='soft-rounded' colorScheme='blue'>
+        <TabList>
+          <Tab>나의 계획</Tab>
+          <Tab>일행과 비교하기</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel width='100%'>
+            <div>
               <p>나의 계획</p>
-            </TabPanel>
-            <TabPanel>
-              <p>일행과 비교하기</p>
-            </TabPanel>
-          </TabPanels>
-        </Tabs>
-      </Container>
-    </Layout>
+              <MapSearch onLocationSelect={handleLocationSelect} />
+              <Map width="500px" height="400px" location={selectedLocation} />
+            </div>
+          </TabPanel>
+          <TabPanel>
+            <p>일행과 비교하기</p>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+    </Container>
   )
 }
