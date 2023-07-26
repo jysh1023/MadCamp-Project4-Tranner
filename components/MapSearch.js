cef24b580130480d30fab4ebeef75101a48cd832
Seonghyun Yang <seonghyuny1023@naver.com>
2023-07-27 02:58:50 +0900

    edit planning page layout

--- components/MapSearch.js
+++ components/MapSearch.js
@@ -1,18 +1,20 @@
 import React, { useState } from "react";
 import axios from "axios";
 import SearchBar from "./SearchBar";
-import { UnorderedList, ListItem, Box } from "@chakra-ui/react";
+import {
+  Flex,
+  Stack,
+  Card,
+  CardBody,
+  Heading,
+  Text,
+  Button,
+} from "@chakra-ui/react";
 
-const Item = () => {
-  return (
-    <Box>
-
-    </Box>
-  )
-}
 
 const MapSearch = ({ onLocationSelect }) => {
-  const [suggestedKeywords, setSuggestedKeywords] = useState([]);
+
+  const [suggestedLocations, setSuggestedLocations] = useState([]);
 
   const handleSearch = async (keyword) => {
 
@@ -27,20 +29,21 @@ const MapSearch = ({ onLocationSelect }) => {
 
         if (response.status === 200) {
           const data = await response.data;
-          setSuggestedKeywords(data.items.map((item) => item.title.replace( /(<([^>]+)>)/ig, '')));
+          setSuggestedLocations(data.items.map((item) => item));
         } else {
-          setSuggestedKeywords([])
+          setSuggestedLocations([]);
         }
       } catch (error) {
         console.error('Error fetching data: ', error);
-        setSuggestedKeywords([]);
+        setSuggestedLocations([]);
       }
     } else {
-      setSuggestedKeywords([]);
+      setSuggestedLocations([]);
     }
   };
 
   const selectLocationHandler = async (address) => {
+    setSuggestedLocations([]);
 
     try {
       const response = await axios.get('/api/naverapi', {
@@ -69,17 +72,48 @@ const MapSearch = ({ onLocationSelect }) => {
 
   };
 
+  const handleAddItem = async() => {
+
+  }
+
+  const SearchItem = ({item, index}) => {
+    const keyword = item.title.replace( /(<([^>]+)>)/ig, '');
+    return (
+      <Card size={'sm'} onClick={()=>selectLocationHandler(keyword)} flexDirection={'row'} align={'center'}>
+        <Flex flexDirection={'column'}>
+          <CardBody>
+            <Heading size='xs'>
+              {item.title.replace(/(<([^>]+)>)/ig, '')}
+            </Heading>
+
+            <Text pt='2' fontSize='sm'>
+              {item.address}
+            </Text>
+          </CardBody>
+        </Flex>
+        <Flex pos={'absolute'} right={'15px'}>
+          <Button
+            colorScheme='teal'
+            type='submit'
+            size={'sm'}
+            onClick={handleAddItem}>
+            추가하기
+          </Button>
+        </Flex>
+
+      </Card>
+    )
+  };
+
   return (
-    <div>
+    <Flex flexDirection={'column'}>
       <SearchBar onSearch={handleSearch} />
-      <UnorderedList>
-        {suggestedKeywords.map((keyword, index) => (
-          <ListItem key={index} onClick={() => selectLocationHandler(keyword)}>
-            {keyword}
-          </ListItem>
+      <Stack spacing={1}>
+        {suggestedLocations.map((item, index,) => (
+          <SearchItem key={index} item={item} />
         ))}
-      </UnorderedList>
-    </div>
+      </Stack>
+    </Flex>
   )
 }
 
